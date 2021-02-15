e364e0e3d26f3a626d64203de31a8784e05a7c0c
Sven Wrieden <wrieden@googlemail.com>
2021-02-15 20:48:04 +0100

    added kicad project init script

---
+++ scripts/setup_project.sh
@@ -0,0 +1,8 @@
+git submodule add git@github.com:wRieDen/kicad-personal.git
+git submodule add git@github.com:KiCad/kicad-symbols.git
+git submodule add git@github.com:KiCad/kicad-footprints.git
+#git submodule add git@github.com:KiCad/kicad-packages3D.git 
+
+wget -O .gitignore "https://www.toptal.com/developers/gitignore/api/macos,linux,windows,kicad"
+
+#setup kicad project inside folder, add project symbols, footprints...
